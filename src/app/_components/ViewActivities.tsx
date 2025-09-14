bfd6ca7246f54e2c2d0b1005ecbf67e0c665c019
Salman Khan <kay2dan@gmail.com>
2025-09-14 12:24:24 +0100

    cleanup: console.log

--- src/app/_components/ViewActivities.tsx
+++ src/app/_components/ViewActivities.tsx
@@ -21,8 +21,6 @@ export default function ViewActivities({
     [habitId, startDate, endDate]
   );
 
-  console.log("activity for habitId: ", habitId, activities);
-
   return (
     <div className="p-6">
       <h4 className="text-xl font-semibold text-gray-800 mb-4">
